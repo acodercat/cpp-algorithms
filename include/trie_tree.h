43625a2f9b7a290c8e48710027cf89b93c4c1faa
codercat <1067302838@qq.com>
2019-04-17 11:02:24 +0800

    trieTree

--- include/trie_tree.h
+++ include/trie_tree.h
@@ -13,10 +13,37 @@ class TrieTree {
 
 private:
     typedef struct Node {
+
+    public:
         int v;
+        char c;
         bool isKey;
         Node *next = NULL;
-        Node *childNodes[];
+        short int childNodeNum = 0;
+        Node(char c) {
+            this->c = c;
+        }
+
+        Node() {
+
+        }
+
+        Node *findChildNode(char c) {
+            for (int i = 0; i < this->childNodeNum; i ++) {
+                if (this->childNodes[i]->c == c) {
+                    return this->childNodes[i];
+                }
+            }
+            return NULL;
+        }
+
+        void addChildNode(Node *node) {
+            this->childNodes[this->childNodeNum] = node;
+            this->childNodeNum ++;
+        }
+    private:
+        // soft array
+        Node *childNodes[1];
 
     } Node;
 
@@ -25,11 +52,41 @@ private:
 
 public:
     TrieTree() {
-        this->rootNode = NULL;
+        this->rootNode = new Node;
     }
 
-    void insert(string key, string value) {
-        Node *currentNode
+    void insert(string key, int v) {
+        Node *currentNode = this->rootNode;
+        for(int i = 0; i <= key.length(); i ++) {
+            if (Node *node = currentNode->findChildNode(key[i])) {
+                currentNode = node;
+                continue;
+            }
+            Node *newNode = new Node(key[i]);
+            currentNode->addChildNode(newNode);
+            currentNode = newNode;
+        }
+
+        if (!currentNode->isKey) {
+            this->size ++;
+            currentNode->isKey = true;
+        }
+        currentNode->v = v;
+    }
+
+    Node *find(string key) {
+        Node *currentNode = this->rootNode;
+        for(int i = 0; i <= key.length(); i ++) {
+            if (Node *node = currentNode->findChildNode(key[i])) {
+                currentNode = node;
+                continue;
+            }
+            return NULL;
+        }
+        if (currentNode->isKey) {
+            return currentNode;
+        }
+        return NULL;
     }
 
 };
--- src/demo/trie_tree.cpp
+++ src/demo/trie_tree.cpp
@@ -4,9 +4,14 @@
 
 #include <iostream>
 #include "trie_tree.h"
+
 using namespace std;
 
 int main() {
-    string str = "adasdad";
-    cout<<str.length()<<endl;
+    TrieTree *trieTree = new TrieTree();
+    trieTree->insert("cat", 12121);
+    trieTree->insert("cat", 3214234);
+    trieTree->insert("cat1212", 555555);
+    cout<<trieTree->find("cat1212")->v<<endl;
+
 }
