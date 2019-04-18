bcb0f572c11a4887afc0fead461482b8b9bca84f
codercat <1067302838@qq.com>
2019-04-18 19:29:24 +0800

    add circular_linked_list

--- include/circular_linked_list.h
+++ include/circular_linked_list.h
@@ -7,18 +7,59 @@
 
 #include <cstring>
 #include <assert.h>
-#include <string>
 
+using namespace std;
 
 class CircularLinkedList {
 private:
     typedef struct Node {
         int v;
-        Node *next;
+        Node *next = NULL, *prev = NULL;
     } Node;
-    Node *head = NULL;
-    unsigned int size = 0;
 
+    Node *dummyHead = NULL;
+    unsigned int size = 0;
+public:
+    CircularLinkedList() {
+        this->size = 0;
+        this->dummyHead = new Node;
+    }
+
+    void insert(int v) {
+        Node *newNode = new Node;
+        newNode->v = v;
+        Node **head = &(this->dummyHead->next);
+        Node **tail = &(this->dummyHead->prev);
+        if (NULL == *head) {
+            newNode->prev = this->dummyHead;
+            newNode->next = this->dummyHead;
+            *head = newNode;
+        } else {
+            newNode->prev = *tail;
+            (*tail)->next = newNode;
+            newNode->next = this->dummyHead;
+        }
+        *tail = newNode;
+        this->size ++;
+    }
+
+    unsigned int getSize() {
+        return this->size;
+    }
+
+    bool isEmpty() {
+        return 0 == this->getSize();
+    }
+
+    string toString() {
+        Node *currentNode = this->dummyHead->next;
+        string str = "head";
+        while(this->dummyHead != currentNode) {
+            str = str + "->" + to_string(currentNode->v);
+            currentNode = currentNode->next;
+        }
+        return str;
+    }
 };
 
 
--- src/demo/circular_linked_list.cpp
+++ src/demo/circular_linked_list.cpp
@@ -6,5 +6,10 @@
 #include "circular_linked_list.h"
 
 int main() {
-
+    CircularLinkedList *circularLinkedList = new CircularLinkedList();
+    circularLinkedList->insert(67);
+    circularLinkedList->insert(31);
+    circularLinkedList->insert(18);
+    circularLinkedList->insert(28);
+    std::cout << circularLinkedList->toString() << std::endl;
 }
