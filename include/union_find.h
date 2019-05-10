e04bcc8197e7662c285272927feb1a821dbec988
codercat <1067302838@qq.com>
2019-05-10 11:59:00 +0800

    add union find

---
+++ include/union_find.h
@@ -0,0 +1,22 @@
+//
+// Created by codercat on 19-5-10.
+//
+
+#ifndef CPP_ALGORITHMS_UNION_FIND_H
+#define CPP_ALGORITHMS_UNION_FIND_H
+
+class UnionFind {
+private:
+    unsigned int size = 0;
+public:
+    UnionFind() {
+
+    }
+
+
+
+};
+
+
+
+#endif //CPP_ALGORITHMS_UNION_FIND_H
---
+++ src/demo/union_find.cpp
@@ -0,0 +1,11 @@
+//
+// Created by codercat on 19-4-18.
+//
+
+#include <iostream>
+#include "union_find.h"
+
+int main() {
+
+
+}
